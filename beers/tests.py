c4b83b15b745993070fd24223c50888e8886000a
AiceFirst <Artur_lakizo@mail.ru>
2016-01-30 21:40:54 +0300

    (Test)

--- beers/tests.py
+++ beers/tests.py
@@ -5,12 +5,15 @@ from datetime import datetime, timedelta
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TsCh.settings")
 
+class PostTestCase(TestCase):
+      def setUp(self):
+                    me = User(username='tete', password='123')
+                    me.save()
+                    Post.objects.create(author=me, title='Truly Delete', text='Delete Dis', published_date=datetime(2012, 3, 1, 10, 0))
 
-
-
-me = User.objects.get(username='tete')
-Post.objects.create(author=me, title='Truly Delete', text='Delete Dis', published_date=datetime(2012, 3, 1, 10, 0))
-post = Post.objects.get(title='Truly Delete')
-post.publish()
-Post.objects.filter(created_date__gte=(datetime.now() - timedelta(days=1))).delete()
+      def test_animals_can_speak(self):
+           post = Post.objects.get(title='Truly Delete')
+           post.publish()
+           Post.objects.filter(created_date__gte=(datetime.now() - timedelta(days=1))).delete()
+           self.assertEqual(Post.objects.all(), [])
 # Create your tests here.
